4a70b3d48698a89c1d50417c46834d07986cd380
Benedetto Simone <sim.benede@gmail.com>
2022-04-23 18:37:59 +0200

    Compute height of tree to use it in fitness function
    Co-authored-by: DanieleSalerno <danysal.f1@gmail.com>

--- EDT/generate_random_tree.py
+++ EDT/generate_random_tree.py
@@ -15,6 +15,7 @@ def generate_random_tree(min_depth, max_depth, num_of_classes, num_features, min
 
         if node_type is NodesTypes.LEAF:
             root = Leaf(num_of_classes)
+            root.set_height_tree(1)
 
         elif node_type is NodesTypes.DECISION:
             rule = Rule(num_features, min_max)
@@ -23,6 +24,14 @@ def generate_random_tree(min_depth, max_depth, num_of_classes, num_features, min
             # add child to root
             root.add_child(tree_node(1, min_depth, max_depth, num_of_classes, num_features, min_max))
             root.add_child(tree_node(1, min_depth, max_depth, num_of_classes, num_features, min_max))
+
+            # get children to compute height of tree
+            children = root.get_children()
+            height = max(children[0].get_height_tree(), children[1].get_height_tree())
+
+            print("Compute max", children[0].get_height_tree(), children[1].get_height_tree())
+            root.set_height_tree(height)
+            print("Tree height", root.get_height_tree())
 
         else:
             raise ValueError('Enum type error: {}'.format(type))
@@ -34,19 +43,45 @@ def tree_node(depth, min_depth, max_depth, num_of_classes, num_features, min_max
 
     if depth == max_depth:
         node = Leaf(num_of_classes)
-    elif min_depth <= depth <= max_depth:
+        node.set_height_tree(1)
+        print("1-Leaf height", node.get_height_tree())
+
+    elif depth < min_depth:
+
+        rule = Rule(num_features, min_max)
+        node = Decision(rule)
+        depth = depth + 1
+        node.add_child(tree_node(depth, min_depth, max_depth, num_of_classes, num_features, min_max))
+        node.add_child(tree_node(depth, min_depth, max_depth, num_of_classes, num_features, min_max))
+        children = node.get_children()
+        height = 1 + max(children[0].get_height_tree(), children[1].get_height_tree())
+
+        print("1-Compute max", children[0].get_height_tree(), children[1].get_height_tree() )
+        node.set_height_tree(height)
+        print("1-Decision height", node.get_height_tree())
+
+    elif min_depth <= depth < max_depth:
 
         # choose random if the next node will be a Decision or a Leaf
         node_type = random.choices(list(NodesTypes))[0]
 
         if node_type is NodesTypes.LEAF:
             node = Leaf(num_of_classes)
+            node.set_height_tree(1)
+            print("2-Leaf height", node.get_height_tree())
 
         elif node_type is NodesTypes.DECISION:
             rule = Rule(num_features, min_max)
             node = Decision(rule)
-            node.add_child(tree_node(depth + 1, min_depth, max_depth, num_of_classes, num_features, min_max))
-            node.add_child(tree_node(depth + 1, min_depth, max_depth, num_of_classes, num_features, min_max))
+            depth = depth + 1
+            node.add_child(tree_node(depth, min_depth, max_depth, num_of_classes, num_features, min_max))
+            node.add_child(tree_node(depth, min_depth, max_depth, num_of_classes, num_features, min_max))
+
+            children = node.get_children()
+            height = 1 + max(children[0].get_height_tree(), children[1].get_height_tree())
+            print("2-Compute max", children[0].get_height_tree(), children[1].get_height_tree())
+            node.set_height_tree(height)
+            print("2-Decision height", node.get_height_tree())
 
         else:
             raise ValueError('Enum type error: {}'.format(type))
--- EDT/tree.py
+++ EDT/tree.py
@@ -13,10 +13,9 @@ class NodesTypes(Enum):
     LEAF = 0
     DECISION = 1
 
+
 LEFT_CHILD = 0
 RIGH_CHILD = 1
-
-
 
 
 class Leaf:
@@ -25,7 +24,7 @@ class Leaf:
         self._score = 0.0
         self._result_class = random.randrange(num_of_classes)
         self._num_of_classes = num_of_classes
-        self._depthTree = 0
+        self._height_tree = 0
 
     # Getter and Setter Method
     def set_score(self, score):
@@ -40,30 +39,30 @@ class Leaf:
     def get_result(self):
         return self._result
 
-    def set_depthTree(self, depthTree):
-        self._depthTree = depthTree
+    def set_height_tree(self, height_tree):
+        self._height_tree = height_tree
 
-    def get_depthTree(self):
-        return self._depthTree
+    def get_height_tree(self):
+        return self._height_tree
 
 
-#rule is the class used to chose how we do the decision#
+# rule is the class used to chose how we do the decision#
 class Rule:
-    def __init__(self,num_features,min_max):
-        #chose a random number in number of feature to make a decision on this feature
-        self._index=random.randrange(num_features)
+    def __init__(self, num_features, min_max):
+        # chose a random number in number of feature to make a decision on this feature
+        self._index = random.randrange(num_features)
 
-        #computing the treshold
-        self._treshold=random.uniform(min_max[self._index][0],min_max[self._index][1])
-        self._num_features=num_features
-        self._min_max=min_max
+        # computing the treshold
+        self._treshold = random.uniform(min_max[self._index][0], min_max[self._index][1])
+        self._num_features = num_features
+        self._min_max = min_max
 
-    #Getter and Setter
-    def set_index(self,index):
-        self._index=index
+    # Getter and Setter
+    def set_index(self, index):
+        self._index = index
 
-    def set_treshold(self,treshold):
-        self._treshold=treshold
+    def set_treshold(self, treshold):
+        self._treshold = treshold
 
     def get_index(self):
         return self._index
@@ -73,29 +72,29 @@ class Rule:
 
 
 class Decision():
-    #contructor#
-    def __init__(self,rule):
-        #contain the childs of the node
+    # contructor#
+    def __init__(self, rule):
+        # contain the childs of the node
         self._children = []
-        self._rule=rule
-        self._score=0.0
-        self._depthTree = 0
+        self._rule = rule
+        self._score = 0.0
+        self._height_tree = 0
 
-    #Getter and Setter
-    def set_score(self,score):
+    # Getter and Setter
+    def set_score(self, score):
         self._score = score
 
     def get_score(self):
         return self._score
 
-    def set_depthTree(self, depthTree):
-        self._depthTree = depthTree
+    def set_height_tree(self, height_tree):
+        self._height_tree = height_tree
 
-    def get_depthTree(self):
-        return self._depthTree
+    def get_height_tree(self):
+        return self._height_tree
 
     def add_child(self, new_child):
         self._children.append(new_child)
 
-
-
+    def get_children(self):
+        return self._children
