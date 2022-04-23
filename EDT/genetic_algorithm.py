116fc6724390e4ddee6a48b235568043f42321f4
DanieleSalerno <72736111+DanieleSalerno@users.noreply.github.com>
2022-04-23 13:06:46 +0200

    Genetic algorithm (generate starting population and a way to evaluate the tree (TO DO))
    Co-authored-by: benedettosimone <sim.benede@gmail.com>

--- EDT/genetic_algorithm.py
+++ EDT/genetic_algorithm.py
@@ -1,27 +1,67 @@
-class GeneticAlgorithm:
+import numpy as np
+from sklearn.metrics import accuracy_score
 
-    def __init__(self, population_size, n_epochs):
-        self.population_size = population_size
-        self.n_epochs = n_epochs
-        self.X_train = None
-        self.y_train = None
+from EDT.generate_random_tree import generate_random_tree
+
+
+class GeneticAlgorithm:
+    #constructor
+    def __init__(self, population_size, n_epochs,min_depth, max_depth):
+        self._population_size = population_size
+        self._n_epochs = n_epochs
+        self._X_train = None
+        self._y_train = None
+        self._min_depth=min_depth
+        self._max_depth=max_depth
+        self._num_of_classes=None
+        self._num_features=None
+        self._min_max= None
 
     def fit(self, X_train, y_train, stop_after_no_progress):
-        self.X_train = X_train
-        self.y_train = y_train
-
+        self._X_train = X_train
+        self._y_train = y_train
 
         # generate starting population
+        population = self.__create_starting_population()
 
         # find best individual to see progress
-
         '''
         repeat for num_epochs
             generate new population with selection, crossover and mutation
             find best individual
             compare it with current best individuals
             exit with stopping condition (>num_epoch, >epoch_no_progress
-            
         return all best individuals 
-            
         '''
+    #function for create the starting population
+    def __create_starting_population(self):
+        population = []
+
+        for i in range(self._population_size):
+            # create new random tree
+            new_tree = generate_random_tree(self._min_depth,self._max_depth,self._num_of_classes,self._num_features,self._min_max)
+            #Evaulating the tree
+            self.__evaluate_tree(new_tree)
+            #Adding the tree to the population
+            population.append(new_tree)
+        return population
+
+    #function that allow us to evaluate a tree
+    def __evaluate_tree(self,tree):
+        # compute a fitness score by approriately weighting accuracy and depth
+        # lower the number, the better
+        alpha1 = 0.99  # penalty for misclassification
+        alpha2 = 0.01  # penalty for large trees
+
+        #predicted label
+        Y_pred = np.apply_along_axis(tree.pred, axis=1, arr=self._X_train)
+
+        accuracy = accuracy_score(self._Y_train, Y_pred)
+        #TO DO DEFINE DEPTH SCORE
+        depth_score = 1
+
+        fitness_score = (alpha1 * accuracy) + (alpha2 * (depth_score))
+
+        tree.set_score()
+        print(self._Y_train)
+        print(Y_pred)
